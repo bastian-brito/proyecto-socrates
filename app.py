2216a71807356b49536a038f80edf4af66f89ea1
fco-villegas <77493999+fco-villegas@users.noreply.github.com>
2021-01-26 16:36:32 -0300

    Update app.py
    Se crearon las primeras 2 tablas del modelo de datos

--- app.py
+++ app.py
@@ -7,6 +7,28 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:@localhost/flask'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+class Rol_Aplicacion(db.Model):
+    __tablename__ = "roles_aplicacion"
+    id = db.Column(db.Integer, primary_key=True)
+    nombres = db.Column(db.String(30), nullable=False)
+    descripcion = db.Column(db.String(60), nullable=False)
+    estado = db.Column(db.Boolean, nullable=False)
+    usuarios = db.relationship('Usuario', backref='roles_aplicacion', lazy=True)
+    
+
+class Usuario(db.Model):
+    __tablename__ = "usuarios"
+    id = db.Column(db.Integer, primary_key=True)
+    nombres = db.Column(db.String(60), nullable=False)
+    fk_rol = db.Column(db.Integer, db.ForeignKey('roles_aplicacion.id'), nullable=False)
+    apellido_paterno = db.Column(db.String(30), nullable=False)
+    apellido_materno = db.Column(db.String(30), nullable=False)
+    correo = db.Column(db.String(60), unique=True)
+    contraseña= db.Column(db.String(60), nullable=False)
+    telefono = db.Column(db.Integer, nullable=False)
+    fecha_creacion = db.Column(db.DateTime, default=datetime.now)
+    estado = db.Column(db.Boolean, nullable=False)
+ 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
