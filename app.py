93c5ee1bd4ef2ad8bf60d0c250c8534b24526b7e
bastian-brito <bastian.brito@gmail.com>
2021-01-29 20:05:49 -0300

    modelo y html nuevo_usuario ok

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from Modelo.Rol_Aplicacion import Rol_Aplicacion
-from Modelo.Usuario import Usuario
+from Modelo.Modelos import *
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:@localhost/flask'
@@ -10,44 +9,35 @@ db = SQLAlchemy(app)
 
 
 @app.route("/")
-def inicio():
-    return render_template('Index.html')
-
-@app.route("/ncrs")
-def ncrs():
-    return render_template('ncrs.html')
+def inicio():    
+    return render_template("Index.html")
 
 @app.route("/nuevo_usuario")
-def nuevo_usuario():
-    return render_template('nuevo_usuario.html')
+def nuevo_usuario():    
+    return render_template("nuevo_usuario.html")
 
-#@app.route("/crear", methods=["POST"])
 @app.route("/nuevo_usuario", methods=["POST"])
 def crear_usuario():
-    nombres          = request.form.get("nombres")    
-    fk_rol           = 1
+    nombres = request.form.get("nombres")    
+    fk_rol = 1
     apellido_paterno = request.form.get("apellido_paterno")
     apellido_materno = request.form.get("apellido_materno")
-    correo           = request.form.get("correo")
-    contraseña       = request.form.get("contraseña")
-    telefono         = request.form.get("telefono")
-    estado           = True
-    usuario = Usuario(
-                     nombres          = nombres,
-                     apellido_paterno = apellido_paterno,
-                     fk_rol           = fk_rol,
-                     apellido_materno = apellido_materno,
-                     correo           = correo,
-                     contraseña       = contraseña,
-                     telefono         = telefono,
-                     estado           = estado)
+    correo = request.form.get("correo")
+    contraseña= request.form.get("contraseña")
+    telefono = request.form.get("telefono")
+    estado = True
+    usuario = Usuario(nombres=nombres, apellido_paterno=apellido_paterno,
+                     fk_rol = fk_rol, 
+                     apellido_materno=apellido_materno,
+                     correo=correo,
+                     contraseña=contraseña,
+                     telefono=telefono,
+                     estado=estado)
     db.session.add(usuario)
     db.session.commit()
     return redirect("/")
-
-
-    
+ 
 if __name__ == "__main__":
-        with app.app_context():
-            db.create_all()
-        app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
