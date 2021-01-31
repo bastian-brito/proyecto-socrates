ce06e8f0182d4b62fc541c6e8b66ec2635e138d4
bastian-brito <bastian.brito@gmail.com>
2021-01-31 18:53:20 -0300

    listado y edicion usuarios ok
    Necesita agregar detalles de seguridad, confirmación, etc

--- Modelo/Modelos.py
+++ Modelo/Modelos.py
@@ -11,44 +11,42 @@ from sqlalchemy.orm import backref
 db = SQLAlchemy()
 
 class Rol_Aplicacion(db.Model):
-    __tablename__ = "roles_aplicacion"
-    id = db.Column(db.Integer, primary_key=True)
-    nombres = db.Column(db.String(30), nullable=False)
-    descripcion = db.Column(db.String(60), nullable=False)
+    __tablename__  = "roles_aplicacion"
+    id             = db.Column(db.Integer, primary_key=True)
+    nombres        = db.Column(db.String(30), nullable=False)
+    descripcion    = db.Column(db.String(60), nullable=False)
     fecha_creacion = db.Column(db.DateTime, default=datetime.now)
-    estado = db.Column(db.Boolean, nullable=False)
-    usuarios = db.relationship('Usuario', backref='roles_aplicacion', lazy=True)
+    estado         = db.Column(db.Boolean, nullable=False)
+    usuarios       = db.relationship('Usuario', backref='roles_aplicacion', lazy=True)
 
 
 class Red_Social(db.Model):
     __tablename__ = "redes_sociales"
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(60), nullable=False)    
-    descripcion = db.Column(db.String(60), nullable=False)
-    link = db.Column(db.String(60), nullable=False)    
+    id             = db.Column(db.Integer, primary_key=True)
+    nombre         = db.Column(db.String(60), nullable=False)    
+    descripcion    = db.Column(db.String(60), nullable=False)
+    link           = db.Column(db.String(60), nullable=False)    
     fecha_creacion = db.Column(db.DateTime, default=datetime.now)
-    estado = db.Column(db.Boolean, nullable=False)
-    #usuarios = db.relationship('Usuario', secondary='usuarios_redes_sociales')
-    usuarios = db.relationship('Usuario_Red_Social', backref='redes_sociales', lazy=True)
+    estado         = db.Column(db.Boolean, nullable=False)
+    usuarios       = db.relationship('Usuario_Red_Social', backref='redes_sociales', lazy=True)
 
 class Usuario(db.Model):
     __tablename__ = "usuarios"
-    id = db.Column(db.Integer, primary_key=True)
-    nombres = db.Column(db.String(60), nullable=False)
-    fk_rol = db.Column(db.Integer, db.ForeignKey('roles_aplicacion.id'), nullable=False)     
+    id               = db.Column(db.Integer, primary_key=True)
+    nombres          = db.Column(db.String(60), nullable=False)
+    fk_rol           = db.Column(db.Integer, db.ForeignKey('roles_aplicacion.id'), nullable=False)
     apellido_paterno = db.Column(db.String(30), nullable=False)
     apellido_materno = db.Column(db.String(30), nullable=False)
-    correo = db.Column(db.String(60), unique=True)
-    contraseña= db.Column(db.String(60), nullable=False)
-    telefono = db.Column(db.Integer, nullable=False)
-    fecha_creacion = db.Column(db.DateTime, default=datetime.now)
-    estado = db.Column(db.Boolean, nullable=False)
-    #red_socials = db.relationship(Red_Social, secondary='usuarios_redes_sociales')
-    red_socials = db.relationship('Usuario_Red_Social', backref='usuarios', lazy=True)
+    correo           = db.Column(db.String(60), unique=True)
+    contraseña       = db.Column(db.String(60), nullable=False)
+    telefono         = db.Column(db.Integer, nullable=False)
+    fecha_creacion   = db.Column(db.DateTime, default=datetime.now)
+    estado           = db.Column(db.Boolean, nullable=False)
+    red_socials      = db.relationship('Usuario_Red_Social', backref='usuarios', lazy=True)
 
 class Usuario_Red_Social(db.Model):
-    __tablename__ = "usuarios_redes_sociales"
-    fk_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'), primary_key=True)
-    fk_red_social = db.Column(db.Integer, db.ForeignKey('redes_sociales.id'), primary_key=True)      
+    __tablename__  = "usuarios_redes_sociales"
+    fk_usuario     = db.Column(db.Integer, db.ForeignKey('usuarios.id'), primary_key=True)
+    fk_red_social  = db.Column(db.Integer, db.ForeignKey('redes_sociales.id'), primary_key=True)      
     fecha_creacion = db.Column(db.DateTime, default=datetime.now)
-    estado = db.Column(db.Boolean, nullable=False)
+    estado         = db.Column(db.Boolean, nullable=False)
--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from Modelo.Modelos import *
 
 app = Flask(__name__)
+app.secret_key = "Secret Key"
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:@localhost/flask'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
@@ -16,23 +17,47 @@ def inicio():
 def nuevo_usuario():    
     return render_template("nuevo_usuario.html")
 
+#this is our update route where we are going to update our employee
+@app.route('/update', methods = ['GET', 'POST'])
+def update():
+ 
+    if request.method == 'POST':
+        usuario = Usuario.query.get(request.form.get('id'))
+ 
+        usuario.nombres = request.form['nombres']
+        usuario.apellido_paterno = request.form['apellido_paterno']
+        usuario.correo = request.form['correo']
+        db.session.merge(usuario)
+        db.session.flush()
+        db.session.commit()
+        flash("Usuario actualizado")
+ 
+        return redirect(url_for('lista_usuarios'))
+
+@app.route("/lista_usuarios")
+def lista_usuarios():
+    usuarios = Usuario.query.order_by(Usuario.fecha_creacion.asc()).all() 
+    return render_template("lista_usuarios.html", usuarios=usuarios)
+
+
 @app.route("/nuevo_usuario", methods=["POST"])
 def crear_usuario():
-    nombres = request.form.get("nombres")    
-    fk_rol = 1
+    nombres          = request.form.get("nombres")    
+    fk_rol           = 1
     apellido_paterno = request.form.get("apellido_paterno")
     apellido_materno = request.form.get("apellido_materno")
-    correo = request.form.get("correo")
-    contraseña= request.form.get("contraseña")
-    telefono = request.form.get("telefono")
-    estado = True
-    usuario = Usuario(nombres=nombres, apellido_paterno=apellido_paterno,
-                     fk_rol = fk_rol, 
-                     apellido_materno=apellido_materno,
-                     correo=correo,
-                     contraseña=contraseña,
-                     telefono=telefono,
-                     estado=estado)
+    correo           = request.form.get("correo")
+    contraseña       = request.form.get("contraseña")
+    telefono         = request.form.get("telefono")
+    estado           = True
+    usuario          = Usuario(nombres=nombres,
+                            apellido_paterno=apellido_paterno,
+                            fk_rol = fk_rol, 
+                            apellido_materno=apellido_materno,
+                            correo=correo,
+                            contraseña=contraseña,
+                            telefono=telefono,
+                            estado=estado)
     db.session.add(usuario)
     db.session.commit()
     return redirect("/")
