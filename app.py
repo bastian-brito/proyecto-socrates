0a797723b527f5b743db441f76c5aacb6deb5443
fco-villegas <77493999+fco-villegas@users.noreply.github.com>
2021-02-02 14:28:57 -0300

    Comentarios
    Comentarios y limpieza

--- app.py
+++ app.py
@@ -5,11 +5,18 @@ from formulario import IngresaUsuario
 from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+# Esta X eliminarse esta linea , no hay manejo de sesiones aun
 app.secret_key = "Secret Key"
+
+#Esta por dejar de existir en esta script -> se muda a config.py
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:@localhost/flask'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
+#Manejo de Migraciones de Base de Datos
 migrate = Migrate(app, db)
+
 #Aqui se importa la referencia Blue print de Usuarios
 from Controladores.Usuarios_Controler import usuarios_bp
 app.register_blueprint(usuarios_bp)
@@ -18,36 +25,10 @@ app.register_blueprint(usuarios_bp)
 def inicio():    
     return render_template("Index.html")
 
-# @app.route("/nuevo_usuario")
-# def nuevo_usuario():    
-#     return render_template("nuevo_usuario.html")
-
-# @app.route('/update', methods = ['GET', 'POST'])
-# def update():
- 
-#     if request.method == 'POST' :
-#         usuario = Usuario.query.get(request.form.get('id'))
- 
-#         usuario.nombres = request.form['nombres']
-#         usuario.apellido_paterno = request.form['apellido_paterno']
-#         usuario.correo = request.form['correo']
-#         db.session.merge(usuario)
-#         db.session.flush()
-#         db.session.commit()
-#         flash("Usuario actualizado")
- 
-#         return redirect(url_for('lista_usuarios'))
-
-# @app.route("/lista_usuarios")
-# def lista_usuarios():
-#     usuarios = Usuario.query.order_by(Usuario.fecha_creacion.asc()).all() 
-#     return render_template("lista_usuarios.html", usuarios=usuarios)
-
 @app.route("/lista_roles")
 def lista_roles():
     roles_aplicacion = Rol_Aplicacion.query.order_by(Rol_Aplicacion.fecha_creacion.asc()).all() 
     return render_template("lista_roles.html", roles_aplicacion=roles_aplicacion)
-
 
 @app.route("/nuevo_usuario_wtform", methods = ['GET', 'POST'])
 def registrar_usuario():
@@ -67,29 +48,6 @@ def registrar_usuario():
         flash('Registro completo')
         return redirect("/")
     return render_template('nuevo_usuario_wtform.html', form=form)
-
-
-# @app.route("/nuevo_usuario", methods=["POST"])
-# def crear_usuario():
-#     nombres          = request.form.get("nombres")    
-#     fk_rol           = 1
-#     apellido_paterno = request.form.get("apellido_paterno")
-#     apellido_materno = request.form.get("apellido_materno")
-#     correo           = request.form.get("correo")
-#     contraseña       = request.form.get("contraseña")
-#     telefono         = request.form.get("telefono")
-#     estado           = True
-#     usuario          = Usuario(nombres=nombres,
-#                             apellido_paterno=apellido_paterno,
-#                             fk_rol = fk_rol, 
-#                             apellido_materno=apellido_materno,
-#                             correo=correo,
-#                             contraseña=contraseña,
-#                             telefono=telefono,
-#                             estado=estado)
-#     db.session.add(usuario)
-#     db.session.commit()
-#     return redirect("/")
  
 if __name__ == "__main__":    
     app.run(debug=True)
