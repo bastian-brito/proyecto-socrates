801d90efec887b819ab79b1b00da91c1c2bcdabb
bastian-brito <bastian.brito@gmail.com>
2021-02-03 17:00:18 -0300

    Solucionado lista_roles

--- Controladores/Roles_Aplicacion_Controller.py
+++ Controladores/Roles_Aplicacion_Controller.py
@@ -6,15 +6,15 @@ from flask import render_template, request, redirect,flash
 from app import db
 
 
-# @roles_aplicacion_bp.route("/nuevo_rol_aplicacion", methods = ['GET'])
-# def nuevo_rol_aplicacion():
-#     roles_aplicacion = Rol_Aplicacion.query.order_by(Rol_Aplicacion.fecha_creacion.asc()).all()  
-#     return render_template("usuarios/nuevo_rol_aplicacion.html", roles_aplicacion=roles_aplicacion)
+@roles_aplicacion_bp.route("/lista_roles", methods = ['GET'])
+def lista_roles():
+    roles_aplicacion = Rol_Aplicacion.query.order_by(Rol_Aplicacion.fecha_creacion.asc()).all()  
+    return render_template("lista_roles.html", roles_aplicacion=roles_aplicacion)
 
 @roles_aplicacion_bp.route('/lista_roles', methods = ['GET', 'POST'])
-def update():
+def update_rol():
  
-    if request.method == 'POST' :
+    if request.method == 'POST' and request.form['editar']:
         rol_aplicacion                = Rol_Aplicacion.query.get(request.form.get('id'))        
         rol_aplicacion.nombres        = request.form['nombres']
         rol_aplicacion.descripcion    = request.form['descripcion']
@@ -29,20 +29,20 @@ def update():
         flash("Rol actualizado")
  
         return redirect('/lista_roles')
-    roles_aplicacion = Rol_Aplicacion.query.order_by(Rol_Aplicacion.fecha_creacion.asc()).all() 
-    return render_template("lista_roles.html", roles_aplicacion=roles_aplicacion)
 
-@roles_aplicacion_bp.route('/lista_roles', methods = ['POST'])    
+@roles_aplicacion_bp.route('/crear_rol', methods = ['GET', 'POST'])   
 def crear_rol():
-    nombres        = request.form.get("nombre_nuevo")    
-    descripcion    = request.form.get("descripcion")
-    estado         = True
-    rol_aplicacion = Rol_Aplicacion(nombres=nombres,
+
+    if request.method == 'POST':
+        nombres        = request.form.get("nombre_nuevo")    
+        descripcion    = request.form.get("descripcion_nuevo")
+        estado         = True
+        rol_aplicacion = Rol_Aplicacion(nombres=nombres,
                                     descripcion=descripcion,
                                     estado=estado)
-    db.session.add(rol_aplicacion)
-    db.session.commit()
-    return redirect("/lista_roles")
+        db.session.add(rol_aplicacion)
+        db.session.commit()
+        return redirect("/lista_roles")
 
 # @roles_aplicacion_bp.route("/lista_roles")
 # def lista_roles():
