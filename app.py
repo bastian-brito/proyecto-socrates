2f6c6339fe9fefc6c504b87790fd2d10dc8e057e
bastian-brito <bastian.brito@gmail.com>
2021-03-08 15:04:09 +0000

    web funcional

--- app.py
+++ app.py
@@ -4,17 +4,33 @@ from formulario import IngresaUsuario
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+#app = Flask(__name__, template_folder='/home/bastianbrito/proyecto-socrates/templates')
 app = Flask(__name__)
+app.config["DEBUG"] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 #app.config.from_object(__name__+'.ConfigClass')
 app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
-app.config['SERVER_NAME']='sitio.tld:5000'
+#app.config['SERVER_NAME']='https://bastianbrito.pythonanywhere.com/'
 #app.url_map.default_subdomain = "www"
 
 login_manager = LoginManager(app)
 login_manager.init_app(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:@localhost/flask'
+SQLALCHEMY_DATABASE_URI = "mysql://{username}:{password}@{hostname}/{databasename}".format(
+    username="bastianbrito",
+    password="zxzxzxzx",
+    hostname="bastianbrito.mysql.pythonanywhere-services.com",
+    databasename="bastianbrito$socrates-db",
+)
+
+#app.config['MYSQL_DATABASE_HOST'] = 'bastianbrito.mysql.pythonanywhere-services.com'
+#app.config['MYSQL_DATABASE_USER'] = 'bastianbrito'
+#app.config['MYSQL_DATABASE_PASSWORD'] = 'zxzxzxzx'
+#app.config['MYSQL_DATABASE_DB'] = 'socrates-db'
+#app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+mysqlconnector://bastianbrito:zxzxzxzx@bastianbrito.mysql.pythonanywhere-services.com/socrates-db'
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
 from Modelo.Modelos import *
 migrate = Migrate(app, db)
@@ -36,12 +52,12 @@ app.register_blueprint(escuelas_bp)
 #app.config['SERVER_NAME'] = website_url
 
 @app.route("/")
-def inicio():    
-    return render_template("Index.html")
+def inicio():
+    return render_template('index.html')
 
 # @app.route("/lista_roles")
 # def lista_roles():
-#     roles_aplicacion = Rol_Aplicacion.query.order_by(Rol_Aplicacion.fecha_creacion.asc()).all() 
+#     roles_aplicacion = Rol_Aplicacion.query.order_by(Rol_Aplicacion.fecha_creacion.asc()).all()
 #     return render_template("lista_roles.html", roles_aplicacion=roles_aplicacion)
 
 @app.route("/nuevo_usuario_wtform", methods = ['GET', 'POST'])
@@ -61,7 +77,7 @@ def registrar_usuario():
         usuario.set_password(password)
         usuario.save()
         # Dejamos al usuario logueado
-        login_user(usuario, remember=True)        
+        login_user(usuario, remember=True)
         flash('Registro completo')
         return redirect("/")
     return render_template('nuevo_usuario_wtform.html', form=form)
@@ -69,13 +85,13 @@ def registrar_usuario():
 @login_manager.user_loader
 def load_user(user_id):
     return User.get_by_id(int(user_id))
- 
+
 if __name__ == "__main__":
-    #website_url = '127.0.0.1 localhost.dev:5000'    
+    #website_url = '127.0.0.1 localhost.dev:5000'
     #app.config['SERVER_NAME'] = website_url
     #app.url_map.default_subdomain = "www"
     #website_url = 'vibhu.gfg:5000'
     #app.config['SERVER_NAME'] = website_url
     # app.run(host='127.0.0.1', port=5000, debug=True)
-    
-    app.run(debug=True)
+
+    app.run()
